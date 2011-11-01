f2d178dc94fee88af537e469bea2a8251b2132fa
Maya Saietz <maya@GLaDOS.(none)>
2011-11-01 11:58:55 +0100

    No longer using xml layout

--- src/mi3/erix/ErixActivity.java
+++ src/mi3/erix/ErixActivity.java
@@ -2,12 +2,16 @@ package mi3.erix;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class ErixActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        
+        LinearLayout layout = new LinearLayout(this);
+        
+        setContentView(layout);
     }
 }
