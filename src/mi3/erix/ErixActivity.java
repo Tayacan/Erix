7592daa016a334a1e824ef365493e2154b5bfd64
Maya Saietz <mayasaietz@gmail.com>
2011-11-04 16:40:55 +0100

    Commented all the code.

--- src/mi3/erix/ErixActivity.java
+++ src/mi3/erix/ErixActivity.java
@@ -11,86 +11,93 @@ import android.widget.TextView;
 public class ErixActivity extends Activity implements android.view.GestureDetector.OnGestureListener {
     /** Called when the activity is first created. */
 
-	LinearLayout layout;
-	TextView flingDetector;
-	DrawView drawView; 
+	LinearLayout layout; 
+	TextView flingDetector; // For debugging.
+	DrawView drawView; // For the actual game.
 
-	private GestureDetector gestureScanner;
+	private GestureDetector gestureScanner; // Detects flings/swipes.
 	
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
+        // For the purpose of detecting flings.
         gestureScanner = new GestureDetector(this);
         
+        // Set up the layout.
         layout = new LinearLayout(this);
         layout.setOrientation(LinearLayout.VERTICAL);
         
         flingDetector = new TextView(this);
-        flingDetector.setText("No flings yet.");
+        flingDetector.setText("No flings yet - " + System.currentTimeMillis()); // Debugging
         layout.addView(flingDetector);
         
         drawView = new DrawView(this);
         drawView.setBackgroundColor(Color.WHITE);
         layout.addView(drawView);
         
-        setContentView(layout);
+        setContentView(layout); // We don't care about R.java
     }
 
+	// Make sure to intercept all touch events so we can run our own code.
 	@Override
 	public boolean onTouchEvent(MotionEvent me) {
 		return gestureScanner.onTouchEvent(me);
 	}
 
+	// onFling is called whenever the user swipes a finger over the screen.
 	@Override
-	public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
-			float velocityY) {
+	public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+		// We copy the velocities so we can manipulate them without losing the originals.
 		float xMov = velocityX;
 		float yMov = velocityY;
-		int dir = 1; // Skal bruges senere.
+		
+		// Make sure that we only have positive values for figuring out what axis we're moving on.
 		if(xMov < 0) {
 			xMov *= -1;
 		}
 		if(yMov < 0) {
 			yMov *= -1;
 		}
-		if(xMov >= yMov) {
-			if(velocityX <= 0) {
-				dir = -1; // Skal bruges senere.
+		
+		// Figure out what axis we're moving on.
+		if(xMov >= yMov) { // x axis
+			if(velocityX <= 0) { // Check the original velocity to get the direction
 				flingDetector.setText("Left!");
-				if(drawView.currentX >= 10) {
-					drawView.currentX -= 10;
-					drawView.invalidate();
+				if(drawView.currentX >= 10) { // Stop at the left edge
+					drawView.currentX -= 10; // Give the onDraw function a new x.
+					drawView.invalidate(); // Refresh the screen.
 				}
-			} else {
+			} else { // Right
 				flingDetector.setText("Right!");
-				if(drawView.currentX <= 100) {
-					drawView.currentX += 10;
-					drawView.invalidate();
+				if(drawView.currentX <= 100) { // Stop after 100 pixel - change later.
+					drawView.currentX += 10; // New x value.
+					drawView.invalidate(); // Refresh screen.
 				}
 			}
-		} else {
-			if(velocityY <= 0) {
-				dir = -1; // Skal bruges senere.
+		} else { // y axis
+			if(velocityY <= 0) { // Moving upwards.
 				flingDetector.setText("Up!");
-				if(drawView.currentY >= 10) {
-					drawView.currentY -= 10;
-					drawView.invalidate();
+				if(drawView.currentY >= 10) { // Stop at the top of the screen.
+					drawView.currentY -= 10; // New y value.
+					drawView.invalidate(); // Refresh screen.
 				}
-			} else {
+			} else { // Down.
 				flingDetector.setText("Down!");
-				if(drawView.currentY <= 100) {
-					drawView.currentY += 10;
-					drawView.invalidate();
+				if(drawView.currentY <= 100) { // Stop after 100 pixel - change later.
+					drawView.currentY += 10; // New y value.
+					drawView.invalidate(); // Refresh screen.
 				}
 			}
 		}
-		return true;
+		return true; // Tells the program that yes, we are using this action.
 	}
 	
 	/*
 	 * En bunke ting som skal være med fordi
 	 * vi implementerer OnGestureListener.
+	 * All returns false to show that we're
+	 * not using them for anything.
 	 */
 
 	@Override
