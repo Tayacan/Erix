edd8c9860d3188a9c12d4fa1800918e5cd0311b4
Maya Saietz <mayasaietz@gmail.com>
2011-11-14 15:14:33 +0100

    Lot's of changes here: 1) Optimized the drawing of the line - it no longer lags. 2) Made the first death-condition - you die if you hit your own line. 3) Wrote restart functionality.

--- src/mi3/erix/ErixActivity.java
+++ src/mi3/erix/ErixActivity.java
@@ -1,8 +1,10 @@
 package mi3.erix;
 
+import java.util.ArrayList;
 import java.util.Map;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
@@ -18,11 +20,14 @@ public class ErixActivity extends Activity implements android.view.GestureDetect
     /** Called when the activity is first created. */
 
 	LinearLayout layout; 
+	LinearLayout gameOver;
 	TextView flingDetector; // For debugging.
 	DrawView drawView; // For the actual game.
 	DisplayMetrics metrics = new DisplayMetrics();
 	int screenWidth;
 	int screenHeight;
+	
+	public boolean gameOverCheck = false;
 	
 	// Tileset using constructor v2.
 	private TileSet tileset = new TileSet(screenWidth / 10, screenHeight / 10);
@@ -51,6 +56,19 @@ public class ErixActivity extends Activity implements android.view.GestureDetect
         layout = new LinearLayout(this);
         layout.setOrientation(LinearLayout.VERTICAL);
         
+        // START Game over screen
+        gameOver = new LinearLayout(this);
+        gameOver.setOrientation(LinearLayout.VERTICAL);
+        
+        TextView gameOverText = new TextView(this);
+        gameOverText.setText("Game over!");
+        gameOver.addView(gameOverText);
+        
+        final TextView restart = new TextView(this);
+        restart.setText("Touch screen to restart.");
+        gameOver.addView(restart);
+        // END Game over screen
+        
         // Get the screen size for later
         getWindowManager().getDefaultDisplay().getMetrics(metrics);
         screenWidth = metrics.widthPixels;
@@ -73,6 +91,20 @@ public class ErixActivity extends Activity implements android.view.GestureDetect
     }
 	
 	/*
+	 * This method restarts the app. Should be called by the restart button's onClick method.
+	 */
+	public void reload () {
+		Intent intent = getIntent();
+		overridePendingTransition(0, 0);
+	    intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+	    
+	    finish();
+	    
+	    overridePendingTransition(0, 0);
+	    startActivity(intent);
+	}
+	
+	/*
 	 * This is the thread that moves the object.
 	 * It sends messages, in the form of Runnables,
 	 * to our Handler.
@@ -88,34 +120,70 @@ public class ErixActivity extends Activity implements android.view.GestureDetect
 					} catch (InterruptedException e) {
 						flingDetector.setText("Something went wrong with the... waiting.");
 					}
-					drawHandler.post(new Runnable () {
-
-						@Override
-						public void run() {
-							tileset.tileStatus.put(drawView.currentX / 10 + "x" + drawView.currentY / 10, TileSet.LINE);
-							// Set the coords for tiles that need to be colored gray.
-							for (Map.Entry<String, Integer> entry : tileset.tileStatus.entrySet()) {
-								if(entry.getValue() == TileSet.LINE) {
-									drawView.lineCoords.add(entry.getKey());
+					if(!gameOverCheck) { // Only keep drawing as long as we're still alive.
+						drawHandler.post(new Runnable () {
+	
+							@Override
+							public void run() {
+								if(tileset.tileStatus.get(drawView.currentX / 10 + "x" + drawView.currentY / 10) == null || 
+										tileset.tileStatus.get(drawView.currentX / 10 + "x" + drawView.currentY / 10) == TileSet.EMPTY ||
+										tileset.tileStatus.get(drawView.currentX / 10 + "x" + drawView.currentY / 10) == TileSet.PLAYER) {
+									// We passed through this tile, change it to be a line so it will be drawn.
+									tileset.tileStatus.put(drawView.currentX / 10 + "x" + drawView.currentY / 10, TileSet.LINE);
 								}
+								
+								// Clear out the list of what should be drawn.
+								drawView.lineCoords = new ArrayList<String>();
+								// Set the coords for tiles that need to be colored gray.
+								for (Map.Entry<String, Integer> entry : tileset.tileStatus.entrySet()) {
+									if(entry.getValue() == TileSet.LINE) { // If it's a line...
+										// ...add the chord
+										drawView.lineCoords.add(entry.getKey());
+									}
+								}
+								// Game over - first make sure we're checking a valid tile (not null)
+								if (tileset.tileStatus.get((drawView.currentX + drawView.speed[0]) / 10 + "x" + (drawView.currentY + drawView.speed[1]) / 10) != null) {
+									// Then check if that tile is a line AND we're currently moving
+									if(tileset.tileStatus.get((drawView.currentX + drawView.speed[0]) / 10 + "x"
+											+ (drawView.currentY + drawView.speed[1]) / 10) == TileSet.LINE &&
+											drawView.speed[0] != 0 || drawView.speed[1] != 0) {
+										// If so, we die because we hit our own line.
+										gameOverCheck = true;
+									}
+								} else {
+									//flingDetector.setText((drawView.currentX + drawView.speed[0]) + "x" + (drawView.currentY + drawView.speed[1]));
+								}
+								
+								// Keep us within the limits of the screen (don't move if that will take us outside the screen)
+								if (drawView.currentX + drawView.speed[0] >= 0 && drawView.currentX + drawView.speed[0] <= screenWidth - 10) {
+									drawView.currentX += drawView.speed[0];
+								}
+								if (drawView.currentY + drawView.speed[1] >= 0 && drawView.currentY + drawView.speed[1] <= screenHeight - 10) {
+									drawView.currentY += drawView.speed[1];
+								}
+								// The current position of the player. 
+								tileset.tileStatus.put(drawView.currentX / 10 + "x" + drawView.currentY / 10, TileSet.PLAYER);
+								
+								drawView.invalidate(); // Stuff changed, we should draw it all again.
 							}
 							
-							if (drawView.currentX + drawView.speed[0] >= 0 && drawView.currentX + drawView.speed[0] <= screenWidth - 10) {
-								drawView.currentX += drawView.speed[0];
+						});
+					} else { // If gameOverCheck is true we want to not draw at all, but send a different message to the handler.
+						drawHandler.post(new Runnable () {
+
+							@Override
+							public void run() {
+								// Go to the gameOver screen.
+								setContentView(gameOver);
 							}
-							if (drawView.currentY + drawView.speed[1] >= 0 && drawView.currentY + drawView.speed[1] <= screenHeight - 10) {
-								drawView.currentY += drawView.speed[1];
-							}
-							tileset.tileStatus.put(drawView.currentX / 10 + "x" + drawView.currentY / 10, TileSet.PLAYER);
-							drawView.invalidate(); // Stuff changed, we should draw it all again.
-						}
-						
-					});
+							
+						});
+					}
 				}
 			}
 			
 		};
-		new Thread(runner).start();
+		new Thread(runner).start(); // Start the thread
 	}
 
 	// Make sure to intercept all touch events so we can run our own code.
@@ -142,26 +210,39 @@ public class ErixActivity extends Activity implements android.view.GestureDetect
 		// Figure out what axis we're moving on.
 		if(xMov >= yMov) { // x axis
 			if(velocityX <= 0) { // Check the original velocity to get the direction
-				flingDetector.setText("Left!");
 				drawView.speed[0] = -10;
 				drawView.speed[1] = 0;
 			} else { // Right
-				flingDetector.setText("Right!");
 				drawView.speed[0] = 10;
 				drawView.speed[1] = 0;
 			}
 		} else { // y axis
 			if(velocityY <= 0) { // Moving upwards.
-				flingDetector.setText("Up!");
 				drawView.speed[0] = 0;
 				drawView.speed[1] = -10;
 			} else { // Down.
-				flingDetector.setText("Down!");
 				drawView.speed[0] = 0;
 				drawView.speed[1] = 10;
 			}
 		}
 		return true; // Tells the program that yes, we are using this action.
+	}
+
+	/*
+	 * If the screen is touched while we're at the game over screen,
+	 * reload the application.
+	 */
+	@Override
+	public boolean onDown(MotionEvent e) {
+		// TODO Auto-generated method stub
+		//flingDetector.setText("Down");
+		
+		if(gameOverCheck) {
+			reload();
+			return true;
+		}
+		
+		return false;
 	}
 	
 	/*
@@ -170,13 +251,6 @@ public class ErixActivity extends Activity implements android.view.GestureDetect
 	 * All returns false to show that we're
 	 * not using them for anything.
 	 */
-
-	@Override
-	public boolean onDown(MotionEvent e) {
-		// TODO Auto-generated method stub
-		//flingDetector.setText("Down");
-		return false;
-	}
 	
 	@Override
 	public void onLongPress(MotionEvent e) {
